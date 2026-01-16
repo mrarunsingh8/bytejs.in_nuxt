d0c38a3f9a09e3dbc26299d9b2eb7166cc172783
Arun Singh <mrarunsingh8@gmail.com>
2026-01-16 21:54:03 +0530

    Apply the SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,59 +1,96 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  /**
+   * We keep SSR enabled globally,
+   * but prerender + cache static pages
+   */
+  ssr: true,
+
+  /**
+   * Nitro: Prerender all static pages
+   */
   nitro: {
     prerender: {
-      routes: ['/']
+      routes: [
+        '/',
+        '/json-formatter',
+        '/qr-code-generator',
+        '/byteurl',
+        '/uuid-generator',
+        '/text-case-converter',
+        '/http-header-checker'
+      ]
     }
   },
+
+  /**
+   * Nuxt modules
+   */
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
     '@nuxtjs/seo'
   ],
+
+  /**
+   * Global <head> config
+   */
   app: {
     pageTransition: { name: 'page', mode: 'in-out' },
+
     head: {
+      /**
+       * Preconnect improves GTM performance
+       */
       link: [
         {
           rel: 'preconnect',
           href: 'https://www.googletagmanager.com'
-        },
-        {
-          rel: 'canonical',
-          href: 'https://www.bytejs.in/'
         }
       ],
+
+      /**
+       * Google Tag Manager (non-blocking)
+       */
       script: [
         {
-          innerHTML: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TKQRN7S5');
-          `,
-          type: 'text/javascript'
+          src: 'https://www.googletagmanager.com/gtm.js?id=GTM-TKQRN7S5',
+          async: true
         }
       ],
+
+      /**
+       * GTM noscript
+       */
       noscript: [
         {
-          innerHTML: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKQRN7S5"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-          `
+          children:
+            '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKQRN7S5" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
         }
       ]
     }
   },
 
+  /**
+   * SEO module base config
+   */
   site: {
-    url: 'https://www.bytejs.in'
+    url: 'https://www.bytejs.in',
+    name: 'ByteJS',
+    description:
+      'ByteJS provides free online developer tools like JSON formatter, QR code generator, URL shortener, and more.'
   },
 
+  /**
+   * Sitemap generation
+   */
   sitemap: {
     strictNuxtContentPaths: true
   },
 
+  /**
+   * Robots.txt
+   */
   robots: {
     rules: [
       {
@@ -64,15 +101,45 @@ export default defineNuxtConfig({
     sitemap: 'https://www.bytejs.in/sitemap.xml'
   },
 
-  devtools: {
-    enabled: true
-  },
-
-  css: ['~/assets/css/main.css'],
-
+  /**
+   * Route-level rules
+   */
   routeRules: {
-    '/': { prerender: true },
-    // Override hosting default "content-signal" header on robots.txt
+    /**
+     * Static + cached pages
+     */
+    '/': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+    '/json-formatter': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+    '/qr-code-generator': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+    '/byteurl': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+    '/uuid-generator': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+    '/text-case-converter': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+    '/http-header-checker': {
+      prerender: true,
+      cache: { maxAge: 3600 }
+    },
+
+    /**
+     * Fix malformed robots.txt header from Cloudflare
+     */
     '/robots.txt': {
       headers: {
         'content-signal': ''
@@ -80,8 +147,26 @@ export default defineNuxtConfig({
     }
   },
 
+  /**
+   * Global CSS
+   */
+  css: ['~/assets/css/main.css'],
+
+  /**
+   * Disable devtools in production
+   */
+  devtools: {
+    enabled: false
+  },
+
+  /**
+   * Compatibility lock
+   */
   compatibilityDate: '2025-01-15',
 
+  /**
+   * ESLint config
+   */
   eslint: {
     config: {
       stylistic: {
